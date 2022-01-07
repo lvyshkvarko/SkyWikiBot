867ac17b3c30ad396023cff2176c3e16a77e34e6
lvyshkvarko <96944854+lvyshkvarko@users.noreply.github.com>
2022-01-07 15:00:30 +0300

    Add files via upload

--- SkyWikiBot.py
+++ SkyWikiBot.py
@@ -10,19 +10,21 @@ bot = telebot.TeleBot("5070041001:AAGy0ZQ8uDoIMkdpmgOGDS4QCEnmgL3SaCY", parse_mo
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, message.chat.first_name + ', введите Ваш запрос!')
+    bot.reply_to(message, message.chat.first_name + ', введите Ваш запрос! \n\nНапример: Игра престолов, Grand Theft Auto, Крысиный король, Зона 51, Семь чудес света, Гипотеза симуляции. \n\nДанный бот ищет статьи на wikipedia.org, выдавая краткое содержание запрошенной статьи.')
 
 
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
-    print(wikipedia.search(message.text))
     try:
-        bot.send_message(message.from_user.id, wikipedia.summary(message.text))
-        bot.send_message(message.from_user.id, wikipedia.summary(wikipedia.search(message.text, results=3)))
+        bot.send_message(message.from_user.id, '🔑 Ключевая версия\n\n' + wikipedia.summary(message.text))
+        bot.send_message(message.from_user.id, '🗂️ Версия №1\n\n' + wikipedia.summary(wikipedia.search(message.text, results=2)))
+        bot.send_message(message.from_user.id, '🗂️ Версия №2\n\n' + wikipedia.summary(wikipedia.search(message.text, results=3)))
+        bot.send_message(message.from_user.id, '🗂️ Версия №3\n\n' + wikipedia.summary(wikipedia.search(message.text, results=4)))
+        bot.send_message(message.from_user.id, '🗂️ Версия №4\n\n' + wikipedia.summary(wikipedia.search(message.text, results=5)))
+        bot.send_message(message.from_user.id, '🗂️ Версия №5\n\n' + wikipedia.summary(wikipedia.search(message.text, results=6)))
         bot.send_message(message.chat.id, message.chat.first_name + ', есть еще запросы? Пишите! 🔎')
     except:
-        bot.send_message(message.chat.id, 'Ваш запрос слишком короткий или имеет множество значений (или не имеет вовсе). \n\nПожалуйста, замените его на синоним или используйте другой запрос.')
-
+        bot.send_message(message.chat.id, 'Ваш запрос слишком короткий или имеет множество значений (или не имеет вовсе). \n\nПожалуйста, замените его на синоним или используйте другой запрос. \n\nНапример: Иван (имя), Титаник (фильм)')
 
 print('Запущен')
 bot.polling()
